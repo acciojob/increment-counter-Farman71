0eae564f1d6c718bce53caffec1bab3063012e4b
Farman71 <farmanhussain71@gmail.com>
2025-01-23 16:39:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 //your JS code here. If required.
 let button=document.getElementById("incrementBtn");
 let counter=document.getElementById("counter");
+// function incrementCounter() {
+// 	alert(counter.textContent)
+// 	let cl=parseInt(counter.textContent);
+// 	counter.textContent=cl+1;
+// }
+
 function incrementCounter() {
-	alert(counter.textContent)
-	let cl=parseInt(counter.textContent);
-	counter.textContent=cl+1;
+  let cl = parseInt(counter.textContent);
+  alert(cl); // This will now alert a number, not a string
+  counter.textContent = cl + 1;
 }
