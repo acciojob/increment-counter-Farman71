246dc412e6ec41844a30fbcb6eb2d2bc94815a4b
Farman71 <farmanhussain71@gmail.com>
2025-01-23 16:23:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let button=document.getElementById("btn");
+let counter=document.getElementById("para");
+function incre() {
+	alert(counter.textContent)
+	let cl=parseInt(counter.textContent);
+	counter.textContent=cl+1;
+}
